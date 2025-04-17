cf1e243972d26c60a38a7de1e42dd647311835c8
Jung Florian <jungflorian@outlook.fr>
2025-04-17 16:56:36 +0200

    17/04 Soir

--- kasa/src/Composants/Collapse.jsx
+++ kasa/src/Composants/Collapse.jsx
@@ -1,31 +1,53 @@
 import { Outlet, Link } from "react-router-dom";
 import "../assets/SASS/Collapse.scss";
+import fleche from "../Media/Vector.png";
 
 function Collapse() {
     return (
         <div className="collapse">
-                <div className="collapse__container__title">
+            <div className="collapse__container">
+                <input type="checkbox" id="collapse1" className="collapse__checkbox" />
+                <label htmlFor="collapse1" className="collapse__container__title">
                     <h1>Fiabilité</h1>
+                    <img src={fleche} alt="fleche" className="fleche" />
+                </label>
+                <div className="collapse__content">
+                    <p>Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont réguliérement vérifiées par nos équipes.</p>
                 </div>
-                <div className="collapse__container__content">
-                    <Outlet />
-                </div>
-                <div className="collapse__container__title">
+            </div>
+
+            <div className="collapse__container">
+                <input type="checkbox" id="collapse2" className="collapse__checkbox" />
+                <label htmlFor="collapse2" className="collapse__container__title">
                     <h1>Respect</h1>
+                    <img src={fleche} alt="fleche" className="fleche" />
+                </label>
+                <div className="collapse__content">
+                    <p>La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entrainera une exclusion de notre plateforme.</p>
                 </div>
-                <div className="collapse__container__content">
-                    <Outlet />
-                </div><div className="collapse__container__title">
+            </div>
+
+            <div className="collapse__container">
+                <input type="checkbox" id="collapse3" className="collapse__checkbox" />
+                <label htmlFor="collapse3" className="collapse__container__title">
                     <h1>Service</h1>
+                    <img src={fleche} alt="fleche" className="fleche" />
+                </label>
+                <div className="collapse__content">
+                    <p>La qualité du service est au coeur de notre engagement chez Kasa.Nous veillons à ce que chaque interaction, que ce soit avec nos hotes ou nos locataires, soit empreinte de respect et de bienveillance</p>
                 </div>
-                <div className="collapse__container__content">
-                    <Outlet />
-                </div><div className="collapse__container__title">
+            </div>
+
+            <div className="collapse__container">
+                <input type="checkbox" id="collapse4" className="collapse__checkbox" />
+                <label htmlFor="collapse4" className="collapse__container__title">
                     <h1>Sécurité</h1>
+                    <img src={fleche} alt="fleche" className="fleche" />
+                </label>
+                <div className="collapse__content">
+                    <p>La sécurité est la priorité de Kasa. Aussi bien pour nos hotes que pour les voyageurs chaque logement correspond aux criteres de sécurité établis par nos services. En laissant une notes aussi bien a l'hote qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurtié domestique pour nos hotes </p>
                 </div>
-                <div className="collapse__container__content">
-                    <Outlet />
-                </div>
+            </div>
         </div>
     );
 }
---
+++ kasa/src/Composants/erreur404.jsx
@@ -0,0 +1,12 @@
+import { Outlet, Link } from "react-router-dom";
+import "../assets/SASS/erreur404.scss";
+
+function Erreur404() {
+    return (
+        <div className="erreur404">
+            <h1>404</h1>
+            <p>Oups! La page que vous demandez n'existe pas.</p>
+            <Link to="/">Retourner sur la page d'accueil</Link>
+        </div>
+    );
+}
